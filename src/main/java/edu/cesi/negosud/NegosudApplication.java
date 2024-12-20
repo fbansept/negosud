a6908005cd028739197812cea0634c8bda5344e0
fbansept <bansept.franck@gmail.com>
2024-12-20 09:54:54 +0100

    Initial commit

---
+++ src/main/java/edu/cesi/negosud/NegosudApplication.java
@@ -0,0 +1,13 @@
+package edu.cesi.negosud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegosudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NegosudApplication.class, args);
+	}
+
+}
